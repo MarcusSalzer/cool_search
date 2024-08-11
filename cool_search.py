a157fac025a7992c4a045b6bb52efc4f658f03d2
Marcus <marcus.salzer@hotmail.se>
2024-08-11 20:46:37 +0200

    KISS

--- cool_search.py
+++ cool_search.py
@@ -3,184 +3,195 @@ from typing import Literal
 
 import numpy as np
 import polars as pl
-
+from plotly import graph_objects as go
+from scipy import stats
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process import kernels as gpk
 from sklearn.neighbors import KernelDensity
-from sklearn.gaussian_process import GaussianProcessRegressor, kernels as gpk
 
 
-# idea: a convenient and smooth hyperparameter search
-
-
-class search_one_dim:
-    """Optimization of a single parameter, optimizing a single metric"""
+class CoolSearch:
+    """Minimization of a black box function."""
 
     __slots__ = [
-        "estimator",
+        "objective",
+        "dtype",
         "fixed_params",
-        "metric",
-        "metric_name",
-        "param_name",
+        "ndim",
         "param_range",
+        "param_types",
         "samples",
-        "X_train",
-        "X_val",
-        "Y_train",
-        "Y_val",
     ]
-
-    @staticmethod
-    def MSE_score(y_true: np.ndarray, y_pred: np.ndarray):
-        if y_true.shape != y_pred.shape:
-            raise ValueError("Need arrays of same shape")
-
-        return np.average((y_true - y_pred) ** 2).item()
-
-    @staticmethod
-    def MAE_score(y_true: np.ndarray, y_pred: np.ndarray):
-        if y_true.shape != y_pred.shape:
-            raise ValueError("Need arrays of same shape")
-
-        return np.average(abs(y_true - y_pred)).item()
-
-    @staticmethod
-    def accuracy_score(y_true: np.ndarray, y_pred: np.ndarray):
-        if y_true.shape != y_pred.shape:
-            raise ValueError("Need arrays of same shape")
-
-        return np.average(y_true == y_pred)
-
-    METRICS = {
-        "MSE": MSE_score,
-        "MAE": MAE_score,
-        "accuracy": accuracy_score,
-    }
 
     def __init__(
         self,
-        estimator,
-        param_name: str,
-        param_range: tuple,
-        X_train: np.ndarray,
-        Y_train: np.ndarray,
-        X_val: np.ndarray | None = None,
-        Y_val: np.ndarray | None = None,
-        metric: Literal["MSE", "MAE", "accuracy"] = "MSE",
+        objective,
+        param_range: dict,
+        param_types: dict[str, str] | None = None,
         fixed_params={},
+        dtype=pl.Float64,
     ) -> None:
-        if X_val is None or Y_val is None:
-            raise NotImplementedError("requires validation data (for now).")
+        """Tools for minimizing a function.
 
-        self.estimator = estimator
-        self.fixed_params = fixed_params
-        self.param_name = param_name
+        ## parameters
+        - objective (callable):
+
+        """
+
+        # set constants
+        self.objective = objective
         self.param_range = param_range
-        self.X_train = X_train
-        self.Y_train = Y_train
-        self.X_val = X_val
-        self.Y_val = Y_val
-        self.metric_name = metric
-        self.metric = self.METRICS[metric]
-        self.samples = pl.DataFrame(
-            schema={param_name: pl.Float64, metric: pl.Float64, "runtime": pl.Float64}
+        self.fixed_params = fixed_params
+
+        if param_types is None:
+            param_types = dict.fromkeys(param_range.keys(), dtype)
+        self.param_types = param_types
+
+        self.ndim = len(self.param_range)
+        self.dtype = dtype
+
+        schema = {param: self.dtype for param in param_range.keys()}
+        schema["score"] = self.dtype
+        schema["runtime"] = self.dtype
+
+        self.samples = pl.DataFrame(schema=schema)
+
+    def __str__(self):
+        return "\n".join(
+            [
+                f"{self.ndim} dimensional search",
+                f"  - {len(self.samples)} samples",
+            ],
         )
 
-    def objective(self, param_value) -> float:
-        """Fit estimator with `param_value` and evaluate and score using `self.metric`."""
+    @property
+    def param_names(self):
+        return list(self.param_range.keys())
+
+    # def get_param_values(self, factors: pl.DataFrame | np.ndarray):
+    #     """Compute actual parameter values from factor in [0,1]."""
+
+    #     try:
+    #         names = factors.columns
+    #     except AttributeError:
+    #         names = self.param_names
+
+    #     factors = factors.reshape(-1, self.ndim)
+    #     N = factors.shape[0]
+
+    #     pr = self.param_range
+
+    #     scale = np.vstack((np.array([pr[k][1] - pr[k][0] for k in names]).T,) * N)
+    #     offset = np.vstack((np.array([pr[k][0] for k in names]).T,) * N)
+
+    #     return pl.DataFrame(
+    #         factors * scale + offset, schema={k: self.param_types[k] for k in names}
+    #     )
+
+    def get_grid(self, steps):
+        """Get an evenly spaced grid for all parameters.
+
+        ## Parameters
+        - steps (int): TODO
+
+        ## Returns
+        - grid (ndarray): array of points in parameter space
+        """
+
+        # TODO: handle datatypes
+
+        mesh = np.meshgrid(*[np.linspace(*r, steps) for r in self.param_range.values()])
+        return np.vstack(list(map(np.ravel, mesh))).T
+
+    def make_factor_grid(self, steps):
+        mesh = np.meshgrid(*[np.linspace(0, 1, steps)] * self.ndim)
+        return np.vstack(list(map(np.ravel, mesh))).T
+
+    def grid_search(
+        self,
+        steps=10,
+        target_runtime=None,
+        verbose: Literal[0, 1, 2] = 1,
+    ):
+        """Evaluate objective on an evenly spaced grid"""
+
+        if target_runtime:
+            if self.samples.is_empty():
+                if verbose >= 1:
+                    print("No previous samples. Running 1 initial evaluation")
+                self.random_search(1)
+
+            # choose steps to approximately run for ´target_runtime´ seconds
+            n_samples = target_runtime / self.samples["runtime"].mean()
+            steps = min(int(round(n_samples ** (1 / self.ndim))), 1)
+            if verbose >= 1:
+                print(
+                    "\n".join(
+                        [
+                            f"choose {steps} steps",
+                            f"  -> maximum {steps**self.ndim} samples",
+                        ]
+                    )
+                )
 
         t_start = default_timer()
 
-        model = self.estimator(**self.fixed_params, **{self.param_name: param_value})
-        model.fit(self.X_train, self.Y_train)
-        pred_val = model.predict(self.X_val).reshape(self.Y_val.shape)
-        t_fitpredict = default_timer() - t_start
+        param_names = self.param_names
 
-        score = self.metric(self.Y_val, pred_val)
-        return score, t_fitpredict
+        # TODO fixa det här
+        grid = pl.DataFrame(self.get_grid(steps), schema=param_names)
 
-    def __str__(self) -> str:
-        lines = [
-            "Cool search:",
-            f"  - {self.estimator}",
-            f"  - training data   {self.X_train.shape}, {self.Y_train.shape}",
-            f"  - validation data {self.X_val.shape}, {self.Y_val.shape}",
-            f"  - parameter       {self.param_name} {self.param_range}",
-            f"  - samples         {len(self.samples)}",
-        ]
-
-        return "\n".join(lines)
-
-    def sample_grid(self, resolution=10, verbose=False):
-        pp = np.linspace(
-            self.param_range[0],
-            self.param_range[1],
-            resolution,
+        # avoid previously sampled points
+        grid_new = grid.join(
+            self.samples.select(param_names),
+            on=param_names,
+            how="anti",
         )
 
-        new_samples = []
-        for p in pp:
-            if p in self.samples[self.param_name]:
-                continue
+        if verbose >= 1:
+            print(f"searching {len(grid_new)} new parameter points")
+            if not self.samples.is_empty():
+                est_runtime = len(grid_new) * self.samples["runtime"].mean()
+                print(f"estimated runtime: {est_runtime:.4f} s.")
 
-            if verbose:
-                print(f"{self.param_name} = {p}")
+        # grid_new = grid_new.with_columns(
+        #     pl.struct(param_names)
+        #     .map_elements(lambda row: self.objective(**row), return_dtype=self.dtype)
+        #     .alias("score")
+        # )
+        scores_new = []
+        runtimes_new = []
+        for row in grid_new.iter_rows(named=True):
+            t_run = default_timer()
+            scores_new.append(self.objective(**row, **self.fixed_params))
+            runtimes_new.append(default_timer() - t_run)
 
-            score, time = self.objective(p)
-
-            new_samples.append((p, score, time))
-
-        print(f"computed {len(new_samples)} new samples")
-
-        self.samples.extend(pl.DataFrame(new_samples, self.samples.schema))
-
-        return self.samples
-
-    def sample_point(self, param_value):
-        if param_value in self.samples[self.param_name]:
-            print(f"already sampled {param_value}")
-            return self.samples.row(
-                by_predicate=pl.col(self.param_name) == param_value
-            )[1]
-        else:
-            score, time = self.objective(param_value)
-            self.samples.extend(
-                pl.DataFrame([(param_value, score, time)], self.samples.schema)
-            )
-            return score
-
-    def sample_points_KDE(
-        self,
-        kernel="gaussian",
-        bandwidth=0.1,
-        resolution=100,
-    ):
-        X = self.samples.select(pl.col(self.param_name)).to_numpy()
-
-        kde = KernelDensity(kernel=kernel, bandwidth=bandwidth).fit(X)
-        log_likelihood = kde.score_samples(
-            np.linspace(*self.param_range, resolution).reshape(-1, 1)
+        grid_new = grid_new.with_columns(
+            score=pl.Series(scores_new),
+            runtime=pl.Series(runtimes_new),
         )
-        return np.exp(log_likelihood)
+        self.samples = pl.concat([self.samples, grid_new])
 
-    def estimate_score_GP(self, resolution=100):
-        X = self.samples.select(pl.col(self.param_name)).to_numpy()
-        Y = self.samples[self.metric_name].to_numpy()
+        runtime_sum = sum(runtimes_new)
+        t_overhead = default_timer() - t_start - runtime_sum
+        if verbose >= 1:
+            print(f"total runtime: {runtime_sum:.4f} s + overhead: {t_overhead:.4f} s.")
 
-        # Define kernel (example: Matern kernel + noise term)
-        kernel = gpk.Matern(length_scale=1.0, nu=2.5) + gpk.WhiteKernel(
-            noise_level=1e-1
-        )
-        gp = GaussianProcessRegressor(
-            kernel=kernel, normalize_y=True, n_restarts_optimizer=10
-        )
-        gp.fit(X, Y)
-        pred_mean, pred_std = gp.predict(
-            np.linspace(*self.param_range, resolution).reshape(-1, 1), return_std=True
-        )
-        return pred_mean, pred_std
+        return grid_new
 
-    def binary_search():
-        pass
+    def random_search(self, N, seed=None, verbose: Literal[0, 1, 2] = 1):
+        """Sample N random points."""
+        rng = np.random.default_rng(seed)
 
-    # idea: approx gradient search
-    # idea: genetic search
-    # idea: bayesian search
+        samples = rng.uniform(0, 1, (N, self.ndim))
+        if verbose >= 1:
+            # print(f"searching {len(grid_new)} new parameter points")
+            if not self.samples.is_empty():
+                pass
+                # est_runtime = len(grid_new) * self.samples["runtime"].mean()
+                # print(f"estimated runtime: {est_runtime:.4f} s.")
+
+    def run_sequence():
+        """Specify a 'macro-program' to run multiple searches."""
+        # idea. run sequentially, /and parallel?
+        # allow for conditions to choose next step
---
+++ cool_search_1d.py
@@ -0,0 +1,296 @@
+from timeit import default_timer
+from typing import Literal
+
+import numpy as np
+import polars as pl
+from plotly import graph_objects as go
+from scipy import stats
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process import kernels as gpk
+from sklearn.neighbors import KernelDensity
+
+
+class search_one_dim:
+    """Optimization of a single parameter, optimizing a single metric.
+
+    The idea is that the function is somewhat smooth?"""
+
+    __slots__ = [
+        "estimator",
+        "fixed_params",
+        "metric",
+        "metric_name",
+        "param_name",
+        "param_range",
+        "samples",
+        "score_gp",
+        "X_train",
+        "X_val",
+        "Y_train",
+        "Y_val",
+    ]
+
+    @staticmethod
+    def MSE_score(y_true: np.ndarray, y_pred: np.ndarray):
+        if y_true.shape != y_pred.shape:
+            raise ValueError("Need arrays of same shape")
+
+        return np.average((y_true - y_pred) ** 2).item()
+
+    @staticmethod
+    def MAE_score(y_true: np.ndarray, y_pred: np.ndarray):
+        if y_true.shape != y_pred.shape:
+            raise ValueError("Need arrays of same shape")
+
+        return np.average(abs(y_true - y_pred)).item()
+
+    @staticmethod
+    def accuracy_score(y_true: np.ndarray, y_pred: np.ndarray):
+        if y_true.shape != y_pred.shape:
+            raise ValueError("Need arrays of same shape")
+
+        return np.average(y_true == y_pred)
+
+    METRICS = {
+        "MSE": MSE_score,
+        "MAE": MAE_score,
+        "accuracy": accuracy_score,
+    }
+
+    def __init__(
+        self,
+        estimator,
+        param_name: str,
+        param_range: tuple,
+        X_train: np.ndarray,
+        Y_train: np.ndarray,
+        X_val: np.ndarray | None = None,
+        Y_val: np.ndarray | None = None,
+        metric: Literal["MSE", "MAE", "accuracy"] = "MSE",
+        fixed_params={},
+    ) -> None:
+        if X_val is None or Y_val is None:
+            raise NotImplementedError("requires validation data (for now).")
+
+        self.estimator = estimator
+        self.fixed_params = fixed_params
+        self.param_name = param_name
+        self.param_range = param_range
+        self.X_train = X_train
+        self.Y_train = Y_train
+        self.X_val = X_val
+        self.Y_val = Y_val
+        self.metric_name = metric
+        self.metric = self.METRICS[metric]
+        self.samples = pl.DataFrame(
+            schema={param_name: pl.Float64, metric: pl.Float64, "runtime": pl.Float64}
+        )
+
+    def objective(self, param_value) -> float:
+        """Fit estimator with `param_value` and evaluate and score using `self.metric`."""
+
+        t_start = default_timer()
+
+        model = self.estimator(**self.fixed_params, **{self.param_name: param_value})
+        model.fit(self.X_train, self.Y_train)
+        pred_val = model.predict(self.X_val).reshape(self.Y_val.shape)
+        t_fitpredict = default_timer() - t_start
+
+        score = self.metric(self.Y_val, pred_val)
+        return score, t_fitpredict
+
+    def __str__(self) -> str:
+        lines = [
+            "Cool search:",
+            f"  - {self.estimator}",
+            f"  - training data   {self.X_train.shape}, {self.Y_train.shape}",
+            f"  - validation data {self.X_val.shape}, {self.Y_val.shape}",
+            f"  - parameter       {self.param_name} {self.param_range}",
+            f"  - samples         {len(self.samples)}",
+        ]
+
+        return "\n".join(lines)
+
+    def sample_grid(
+        self,
+        resolution=10,
+        verbose=False,
+        update=True,
+    ):
+        pp = np.linspace(
+            self.param_range[0],
+            self.param_range[1],
+            resolution,
+        )
+
+        new_samples = []
+        for p in pp:
+            if p in self.samples[self.param_name]:
+                continue
+
+            if verbose:
+                print(f"{self.param_name} = {p}")
+
+            score, time = self.objective(p)
+
+            new_samples.append((p, score, time))
+
+        print(f"computed {len(new_samples)} new samples")
+
+        new_df = pl.DataFrame(new_samples, self.samples.schema)
+        if update:
+            self.samples.extend(new_df)
+
+        return new_df
+
+    def sample_point(self, param_value):
+        if param_value in self.samples[self.param_name]:
+            print(f"already sampled {param_value}")
+            return self.samples.row(
+                by_predicate=pl.col(self.param_name) == param_value
+            )[1]
+        else:
+            score, time = self.objective(param_value)
+            self.samples.extend(
+                pl.DataFrame([(param_value, score, time)], self.samples.schema)
+            )
+            return score
+
+    def sample_points_KDE(
+        self,
+        kernel="gaussian",
+        bandwidth=0.1,
+        resolution=100,
+    ):
+        X = self.samples.select(pl.col(self.param_name)).to_numpy()
+
+        kde = KernelDensity(kernel=kernel, bandwidth=bandwidth).fit(X)
+        log_likelihood = kde.score_samples(
+            np.linspace(*self.param_range, resolution).reshape(-1, 1)
+        )
+        return np.exp(log_likelihood)
+
+    def fit_score_GP(self, predict_resolution: int | None = None):
+        X = self.samples.select(pl.col(self.param_name)).to_numpy()
+        Y = self.samples[self.metric_name].to_numpy()
+
+        kernel = gpk.Matern(length_scale=1.0, nu=2.5)
+        self.score_gp = GaussianProcessRegressor(
+            kernel=kernel, normalize_y=True, n_restarts_optimizer=10
+        )
+        self.score_gp.fit(X, Y)
+
+        if predict_resolution:
+            return self.score_gp.predict(
+                np.linspace(*self.param_range, predict_resolution).reshape(-1, 1),
+                return_std=True,
+            )
+
+    def sample_next_GP(
+        self,
+        explore_ratio=0.5,
+        eval_resolution=100,
+        verbose=True,
+        confint=0.95,
+    ):
+        """Pick and sample one point based on the score GP estimate."""
+
+        # if len(self.samples) < 5:
+        #     raise ValueError("Needs more initial samples")
+
+        t_start = default_timer()
+
+        self.fit_score_GP()
+        if verbose:
+            print(f"Fitted GP ({default_timer()-t_start:.4f} seconds)")
+
+        xx = np.linspace(*self.param_range, eval_resolution).reshape(-1, 1)
+
+        pred_mean, pred_std = self.score_gp.predict(
+            xx,
+            return_std=True,
+        )
+        z = stats.norm.ppf(1 - (1 - confint) / 2)
+
+        LCI = pred_mean - z * pred_std
+
+        criterion = (1 - explore_ratio) * (LCI / LCI.max())
+        criterion -= explore_ratio * (pred_std / pred_std.max())
+
+        next_val = xx[np.argmin(criterion)][0]
+
+        print(f"sampling {next_val}")
+
+        t_start = default_timer()
+        self.sample_point(next_val)
+
+        if verbose:
+            print(f"sampled point ({default_timer()-t_start:.4f} seconds)")
+        return criterion
+
+    def plot_score_GP(
+        self,
+        confidence=0.95,
+        resolution=100,
+        high_res_reference: int | None = None,
+    ):
+        try:
+            gp = self.score_gp
+        except AttributeError:
+            raise AttributeError("fit GP first")
+
+        pred_mean, pred_std = gp.predict(
+            np.linspace(*self.param_range, resolution).reshape(-1, 1), return_std=True
+        )
+
+        samples_low = self.samples.clone()
+        if high_res_reference:
+            samples_high = self.sample_grid(high_res_reference, update=False)
+
+        z = stats.norm.ppf(1 - (1 - confidence) / 2)
+
+        fig = go.Figure().add_traces(
+            [
+                go.Scatter(
+                    x=np.linspace(*self.param_range, resolution),
+                    y=pred_mean - z * pred_std,
+                    name="lower CI",
+                    line_color="orange",
+                ),
+                go.Scatter(
+                    x=np.linspace(*self.param_range, resolution),
+                    y=pred_mean + z * pred_std,
+                    name="upper CI",
+                    fill="tonexty",
+                    line_color="orange",
+                ),
+                go.Scatter(
+                    x=samples_low[self.param_name],
+                    y=samples_low[self.metric_name],
+                    name=f"{len(samples_low)} samples",
+                    mode="markers",
+                    marker_size=8,
+                ),
+            ]
+        )
+        if high_res_reference:
+            fig.add_trace(
+                go.Scatter(
+                    x=samples_high[self.param_name],
+                    y=samples_high[self.metric_name],
+                    name=f"{len(samples_high)} samples",
+                    mode="lines",
+                    line_dash="dash",
+                ),
+            )
+        fig.update_xaxes(title="parameter value")
+        fig.update_yaxes(title=self.metric_name)
+        fig.update_layout(title="Gaussian process model")
+        return fig
+
+    def binary_search():
+        pass
+
+    # idea: approx gradient search
+    # idea: genetic search
+    # idea: bayesian search
